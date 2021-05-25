62568a5237e0538ee4dcbfd2a296e6564503537f
abrugiolo <abrugiolosz@gmail.com>
2021-05-25 08:42:17 -0300

    implementa testes AssuntoController

--- src/main/java/br/com/devinhouse/projetofinalmodulo2/dto/AssuntoDtoOutput.java
+++ src/main/java/br/com/devinhouse/projetofinalmodulo2/dto/AssuntoDtoOutput.java
@@ -1,5 +1,6 @@
 package br.com.devinhouse.projetofinalmodulo2.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,10 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AssuntoDtoOutput {
 
     private Integer id;
     private String descricao;
     private String dtCadastro;
     private Character flAtivo;
+
+    @Override
+    public String toString() {
+        return "{" + "id=" + id + ", descricao='" + descricao + '\'' + ", dtCadastro='" + dtCadastro + '\'' + ", flAtivo=" + flAtivo + '}';
+    }
 }
